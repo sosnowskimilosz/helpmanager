4f9475846ddc58250fa0862cd9122262f304aafa
sosnowskimilosz <sosnowskimilosz@gmail.com>
2022-02-14 12:33:26 +0100

    add basic project structure

---
+++ src/main/java/com/example/helpmanager/HelpmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.helpmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpmanagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelpmanagerApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/helpmanager/HelpmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
